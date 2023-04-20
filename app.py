dda506c3a217f0da8ff881b2e32abee0058ff243
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-20 12:42:09 +0100

    set decorators with respect to the URL of the func

--- app.py
+++ app.py
@@ -1,27 +1,83 @@
-from flask import Flask, render_template
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
-from .extensions import db
-from .routes import auth, main, cart
+from typing import Tuple, Union
+from flask import Flask, jsonify, abort, request, redirect
+from auth import Auth
 
-def create_app(config_file='settings.py'):
-    app = Flask(__name__)
-    app.config.from_pyfile(config_file)
-    
-    db.init_app(app)
-    migrate = Migrate(app, db)
-    
-    csrf = CSRFProtect(app)
-    
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(main.bp)
-    app.register_blueprint(cart.bp)
-    
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template('404.html'), 404
+app = Flask(__name__)
 
-    return app
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> Tuple[str, int]:
+    """Registers a new user if it does not exist before"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    response = {"email": email, "message": "user created"}
+    return jsonify(response)
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    '''implements login functionality
+    Returns:
+        str: JSON payload with session_id
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+
+    if not valid_login:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> Union[abort, redirect]:
+    """logout basically means to set the session_id to None
+
+    Returns:
+        Union[abort, redirect]: 403 if session_id or user is None
+        else redirect to '/'
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if session_id is None or user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', strict_slashes=False)
+def profile() -> str:
+    """access profile if session id exists
+    Returns:
+        str: user email
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if session_id is None or user is None:
+        abort(403)
+
+    return jsonify({'email': user.email})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
